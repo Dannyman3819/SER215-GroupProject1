f0db744f33fe38cb08568dd287ab97b3824b439d
pilotaids <pilotaids@hotmail.com>
2017-06-16 08:23:35 -0500

    Cleaned up
    Cleaned up a lot of unnecessary code printing in the compiler and similar things

--- src/HangDictionary.java
+++ src/HangDictionary.java
@@ -1,5 +1,5 @@
 /**
- * Created by MarcoRosa on 6/9/17
+ * Created by MarcoRosa
  * Hangman dictionary class.
  */
 
@@ -14,35 +14,14 @@ public class HangDictionary {
     static File hangmanWordsList;
 
     /**
-     * Main used for testing - PLEASE DO NOT DELETE THE COMMENTED ITEMS! I WILL CLEAN THEM UP ONCE THE CLASS
-     * HAS BEEN FULLY TESTED AND WE REACHED A FINAL VERSION
-     */
-//    public static void main(String[] args) throws IOException{
-//        HangDictionary myDictionary = new HangDictionary();
-//
-////        System.out.println("The size of the dictionaryWords list = "+dictionaryWords.size());
-//        System.out.println("SearchWordFromFile returns "+SearchWordFromFile());
-//        System.out.println("SearchWordFromList returns "+SearchWordFromList());
-//
-//        System.out.println("getWordFromList() returns "+myDictionary.getWordFromList());
-//        System.out.println("getWordFromFile() returns "+myDictionary.getWordFromFile());
-//
-//        System.out.println("Thank you for choosing the Hangman Dictionary service for your dictionary needs." +
-//                "\nHope to serve you again soon :)");
-//    }
-
-    /**
      * Constructor for the HangDictionary class
      * It checks the file path and adjusts it according to the platform used
      * Looks and, if needed, creates the dictionary file
      * Makes the dictionary available as file and as a List<String>
      */
     public HangDictionary() throws IOException {
-//        System.out.println("Calling HandleDirectories()");
         String pathToDictionary = HandleDirectories();
         hangmanWordsList = new File(pathToDictionary);
-//        System.out.println("hangmanWordsList file name is "+hangmanWordsList.getName());
-//        System.out.println("Calling CheckForFile()");
         CheckForFile(hangmanWordsList);
         setDictionaryList();
     }
@@ -56,18 +35,14 @@ public class HangDictionary {
         String path, newPath;
 
         path = referenceCodeFile.getAbsolutePath();
-//        System.out.println("absolute path is "+referenceCodeFile.getAbsolutePath());
         newPath = path.substring(0, (path.length()-19)).concat("HangmanWordsList.txt");
-////        newPath = path.substring(0, (path.length()-19)).concat("TestWordsList.txt");
-//        System.out.println("absolute newPath is "+newPath);
-
         return newPath;
     }
 
     /**
      * Check for a file, if not found it creates it and copies the words from an original one
      * in the src project directory
-     * @param file (cannot use the filed/non-static variable because this method is static)
+     * @param file
      */
     private static void CheckForFile(File file) {
         String pathToReadFile, separator, path;
@@ -76,23 +51,10 @@ public class HangDictionary {
             if (!file.exists()) {
                 file.createNewFile();
                 separator = path.substring((path.length()-21), (path.length()-20));
-////                separator = path.substring((path.length()-18), (path.length()-17));
-//                System.out.println("separator = "+separator);
                 pathToReadFile = path.substring(0,(path.length()-20))+"src"+separator+file.getName();
-////                pathToReadFile = path.substring(0,(path.length()-17))+"src"+separator+file.getName();
-//                System.out.println("pathToReadFile = "+pathToReadFile);
                 File read = new File(pathToReadFile);
-//                System.out.println("read file name is "+read.getName());
-//                System.out.println("Calling ReadWrite()");
                 ReadWrite(read, file);
-//                System.out.println("Opening read file");
-//                OpenFileToScreen(read);
-//                System.out.println("Opening written file");
-//                OpenFileToScreen(file);
-//                System.out.println("File "+file.getName()+" has been created.");
             }
-//            else
-//               System.out.println("File "+file.getName()+" already existed!");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -108,15 +70,8 @@ public class HangDictionary {
         PrintWriter outFile = new PrintWriter (outputStream);
         String lineWord;
 
-//        if(!readFrom.exists())
-//            System.out.println("Does not exist.");
-//
-//        System.out.println("writeTo.getAbsoluteFile() = "+writeTo.getAbsoluteFile());
-        while ((lineWord = inputStream.readLine()) != null) {
-//            System.out.print("Writing\t");
-//            System.out.println("inputStream.readLine() = "+lineWord);
+        while ((lineWord = inputStream.readLine()) != null)
             outFile.println(lineWord);
-        }
 
         outFile.flush();
         outFile.close();
@@ -133,30 +88,19 @@ public class HangDictionary {
         int totCounter = 0, counter = 0, chosenLine;
         String lineWord, chosenWord;
 
-        while ((lineWord = inputStream.readLine()) != null) {
-//            System.out.println("inputStream.readLine() = "+lineWord);
+        while ((lineWord = inputStream.readLine()) != null)
             totCounter++;
-        }
-//        System.out.println("tot number of words = "+totCounter);
+        
         inputStream.close();
-//        System.out.println("inputStream closed");
-
         BufferedReader searchPool = new BufferedReader(new FileReader(hangmanWordsList));
-//        System.out.println("searchPool opened");
-
         chosenLine = findLine.nextInt(totCounter)+1;
-//        System.out.println("the chosen line is "+chosenLine);
+        
         do{
             chosenWord = searchPool.readLine();
-//            System.out.println("searchPool.readLine("+(counter+1)+") = "+chosenWord);
             counter++;
-//            System.out.println("counter = "+counter);
         }while(counter<chosenLine);
-
-//        System.out.println("the chosen word is "+chosenWord);
+        
         searchPool.close();
-//        System.out.println("searchPool closed");
-
         return chosenWord;
     }
 
@@ -188,7 +132,6 @@ public class HangDictionary {
         while ((line = inputStream.readLine()) != null)
             dictionaryWords.add(line);
 
-//        System.out.println("the size of the dictionary = "+dictionaryWords.size());
         inputStream.close();
     }
 
@@ -210,23 +153,21 @@ public class HangDictionary {
     /**
      * Opens the dictionary file in automatic for the user, or instructs the user how to open it
      */
-//    public static void OpenFileToScreen(File file){
+    
     public void OpenFileToScreen(){
         //Check if Desktop functions are supported by Platform
         if(!Desktop.isDesktopSupported()){
             System.out.println("\tDesktop is not supported."
                     +"\t\nPlease open the "+hangmanWordsList.getName()+"file manually with the default text editor."
-//                    + "\t\nPlease open the "+file.getName()+"file manually with the default text editor."
                     + "\t\nThe file is located in the project directory.");
         }
         else{
             try {
                 Desktop.getDesktop().edit(hangmanWordsList);
-//                Desktop.getDesktop().edit(file);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-
+    
 }
--- src/hangman.java
+++ src/hangman.java
@@ -1,4 +1,4 @@
-import java.awt.Window;
+
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.IOException;
@@ -11,58 +11,39 @@ import javax.swing.JOptionPane;
 public class hangman {
 	public static void main (String[] args) throws  InterruptedException, IOException {
 		HangDictionary dictionaryPool = new HangDictionary();
-
-		System.out.println("Program Booting...");
-
-		// prompt to continue with pre-game procedures
-		/*start = JOptionPane.showConfirmDialog(null,"A random word will be generated and the goal of the game is "
-				+ "for you to try and guess all of the letters in that word.\n"
-				+ "Do you wish to proceed?","GAME PLAY CONFIRMATION",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
-
-		if (start == answer)
-			System.out.println("Enter Difficulty Pane...");
-		else{
-			System.out.println("Hangman Exiting...");
-			answer = JOptionPane.NO_OPTION;
-		}*/
-		
 		int playAgain = 0;
 		hangmanGUI gui = new hangmanGUI();
 		
+		System.out.println("Program Booting...");
+		
 		do{
-			//int newWordGen = wordGen.nextInt(dictionary.length), won, wrong = 0;
 			String word = dictionaryPool.getWordFromFile();
-
 			String[] difficulty = {"BEGINNER", "INTERMEDIATE", "EXPERT"};
 			String asterisk = "";
 			int maxMisses;
+			
 			for(int i = 0; i < word.length(); i++)
 				asterisk += '*';
 
-			//hangmanGUI gui = new hangmanGUI();
-
-			// choose level
+			//choose level
 			int choice = gui.promptUser("Please Choose a difficulty", difficulty);
+			
+			//a switch statement would not have worked here, because the break in the else
+			//statement needs to exit the do-loop instead of just the switch :)
 			if (choice == 0) {
-				System.out.println("\nInitiating Hangman\nDifficulty: BEGINNER");
 				maxMisses = asterisk.length() * 2;
 				gui.setDifficulty("BEGINNER");
 			}
 			else if (choice == 1) {
-				System.out.println("\nInitiating Hangman\nDifficulty: INTERMEDIATE");
-				maxMisses = asterisk.length() + 5;
+				maxMisses = asterisk.length() + (int)(asterisk.length()/2);
 				gui.setDifficulty("INTERMEDIATE");
 			}
 			else if (choice == 2) {
-				System.out.println("\nInitiating Hangman\nDifficulty: HARD");
 				maxMisses = asterisk.length();
 				gui.setDifficulty("HARD");
 			}
-			else{
-				System.out.println("\nMaybe next time then.\nBye!");
+			else
 				break;
-			}
-			System.out.println("choice = " + choice);
 			
 			gui.initGUI();
 			gui.setWord(asterisk);
@@ -70,18 +51,17 @@ public class hangman {
 
 			int won = 1, wrong = 0;
 			int guessPerBodyPart = ((maxMisses) / 6);
+			
 			// GAME PLAY
 			while((maxMisses > wrong) && (won > 0)) {
-				Thread.sleep(100);              // Will cause weird issues if some kind of delay isn't used
-				
-				/* 
-				 * TODO COMPLETED - HANDLES THE CLOSING OF THE GAME WINDOW BY PRESSING THE X BUTTON
-				 */
+				//will cause weird issues if some kind of delay isn't used
+				Thread.sleep(100);
+
 				gui.f.addWindowListener(new WindowListener(){
 					public void windowClosing(WindowEvent we) {
 						System.out.println("Closing....");
 					    System.exit(0);
-					  }
+					}
 					@Override
 					public void windowOpened(WindowEvent e) {}
 					@Override
@@ -96,57 +76,35 @@ public class hangman {
 					public void windowDeactivated(WindowEvent e) {}
 				});
 				
-				
-				
 				if (gui.isActionPerformed()) {
-					// getEventValue returns a string of what the user guessed
 					String string = gui.getEventValue();
-					System.out.println("\nKey pressed: " + string);
 					//gui.drawNextBodyPart();
-					//gui.updateWord("_ _ C _");
 
 					won = word.length();
 					boolean missed = true;
-					//System.out.print("\nGuess a letter: ");
 					//guess = keyboard.next();
 					
-					/* 
-					 * TODO COMPLETED - IMPLEMENTATION TO SUPPORT FULL WORD GUESSES
-					 */
-					System.out.println("string.length() = " + string.length());
-					System.out.println("string = word is " 
-							+ (string.compareToIgnoreCase(word) == 0));
-					System.out.println("IF condition = " 
-							+ ((string.length()> 1) && (string.compareToIgnoreCase(word) == 0)));
-					if((string.length()> 1) && (string.compareToIgnoreCase(word) == 0)){
+					if((string.length()> 1) && (string.compareToIgnoreCase(word) == 0))
 						won = 0;
-						System.out.println("inside WHILE condition to cycle again is "
-								+ ((maxMisses > wrong) && (won > 0)));
-					}
 					else{
 						if(string.length() == 1){
 							char guess = string.charAt(0);
 							//maxMisses[wrong] = guess.charAt(0);
 
-							//System.out.println(word);
-							// places asterisks in place of the letters not guessed correctly, yet
+							//places asterisks in place of the letters not guessed correctly, yet
 							for (int i = 0; i < word.length(); i++) {
 								char wordChar = word.charAt(i);
 								wordChar = Character.toUpperCase(wordChar);
 								guess = Character.toUpperCase(guess);
 								if (wordChar == guess) {
-									//asterisk = asterisk.replaceAll(guess, )
 									asterisk = asterisk.substring(0, i) + word.charAt(i) + asterisk.substring(i + 1);
-									//maxMisses[wrong] = '\u0000';
 									missed = false;
 								}
 							}
 
-							if (missed == false) {
+							if (missed == false)
 								gui.setWord(asterisk);
-								System.out.println("Letter " + guess + " IS in the word.");
-							} else {
-								System.out.println("Letter " + guess + " is NOT in the word.");
+							else {
 								wrong++;
 								
 								// TODO Adjust formula here
@@ -155,53 +113,34 @@ public class hangman {
 								if(temp == guessPerBodyPart){
 									gui.drawNextBodyPart();
 								}
-
 							}
 
-							// display for updated word and remaining incorrect guesses
-							System.out.println("Incorrect guesses remaining: " + (maxMisses - wrong));
+							//update GUI with updated word and remaining incorrect guesses
 							gui.setGuessRemain(String.valueOf(maxMisses - wrong));
-							System.out.print("HANGMAN WORD: ");
 							for (int i = 0; i < asterisk.length(); i++) {
-								System.out.print(asterisk.charAt(i));
 								if (asterisk.charAt(i) != '*')
 									won--;
 							}
-							System.out.println();
-							System.out.println("inside WHILE condition to cycle again is "
-									+ ((maxMisses > wrong) || (won > 0)));
 						}
 					}
 				}
 			}
 			
 			if (won == 0) {
-				//answer = JOptionPane.showConfirmDialog(null, "YOU ACTUALLY WON.\nWould you like to play again?","AGAIN?",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
-				System.out.println("You Won");
 				String [] options = {"Yes", "No"};
 				playAgain = gui.promptUser("You Won!!!\nWould you like to play again??", options);
-				/* 
-				 * TODO COMPLETED - IMPLEMENTATION TO GET YES/NO ANSWER FROM USER
-				 */
 			} else if (maxMisses - wrong == 0) {
-				//answer = JOptionPane.showConfirmDialog(null, "YOU LOST.\nWould you like to play again?","AGAIN?",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
-				System.out.println("You Lost");
 				String [] options = {"Yes", "No"};
 				playAgain = gui.promptUser("You Lost!!!\nWould you like to play again??", options);
-				/* 
-				 * TODO COMPLETED - IMPLEMENTATION TO GET YES/NO ANSWER FROM USER
-				 */
 			}
-			
-			System.out.println("playAgain = " + playAgain);
-			System.out.println("JOptionPane.YES_OPTION = " + JOptionPane.YES_OPTION);
 			
 		}while(playAgain == JOptionPane.YES_OPTION);
 		
 		System.out.println("THE END");
 		System.exit(0);
 	}
-
+	
+	/*
 	protected static String getBlanks(String word){
 		String tempStr = "";
 		for(int i=0; i<=word.length(); i++){
@@ -209,4 +148,5 @@ public class hangman {
 		}
 		return tempStr;
 	}
+ 	*/
 }
